9d970b4e21a93b9b065fc44a54f7ec43df0e2094
michael-senft <michael.senft@student.ap.be>
2014-12-17 14:57:23 +0100

    test5

--- js/hello.js
+++ js/hello.js
@@ -6,12 +6,12 @@ function inloggen(id) {
         type: "GET",
         url: "http://ergorestdavid.cloudapp.net/Service1.svc/GetAllUsers",
         dataType: "json",
-        succes: function(data){
+        succes: function (data) {
             document.getElementById('greeting-content').textContent = data.Naam;
         }
-    })
-        
     });
+        
+   
     document.getElementById('greeting-content').style.visibility = 'visible';
     document.getElementById('greeting-id').style.visibility = 'visible';
 }
